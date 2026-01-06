38600ebee494a94b427b31eed2339b2a2c7702e9
Hussain88876 <hassonyemw@gmail.com>
2026-01-06 21:48:27 +1300

    Added pagination for invoice search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,6 +3,8 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
+
+
  
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -10,8 +12,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
+
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
       if (term) {
       params.set('query', term);
     } else {
